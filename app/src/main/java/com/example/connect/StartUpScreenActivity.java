7735ed4f571a7610f99aa0f46cd4476414ddb334
Rawan Emad <rawan.emadnegm@gmail.com>
2021-12-16 14:01:49 +0200

    StartUpScreen initial design

---
+++ app/src/main/java/com/example/connect/StartUpScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.connect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartUpScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_up_screen);
+    }
+}
